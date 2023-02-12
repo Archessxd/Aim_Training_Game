7a1581c0a195abf0ce98662fb154567e003dbdc6
rasit <108332998+Archessxd@users.noreply.github.com>
2023-02-12 14:43:08 +0300

    Aim Training Game

---
+++ aim training/2 - aim training/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2___aim_training
+{
+    public partial class Form1 : Form
+    {
+        string dif;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Class1 c = new Class1();
+                if (radioButton1.Checked)
+                {
+                    dif = "e";
+                    c.Dif = dif;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+                else if (radioButton2.Checked)
+                {
+                    dif = "m";
+                    c.Dif = dif;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+                else if (radioButton3.Checked)
+                {
+                    dif = "h";
+                    c.Dif = dif;
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+
+                else
+                {
+                    MessageBox.Show("plz select a difficulty");
+                }   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
